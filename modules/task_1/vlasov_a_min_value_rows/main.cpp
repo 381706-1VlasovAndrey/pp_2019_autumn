4b5a64e73a71cdd677687f217b450a07777dbb7c
381706-1VlasovAndrey <andrio.vlasov.2000@mail.ru>
2019-10-14 14:22:57 +0300

    First

---
+++ modules/task_1/vlasov_a_min_value_rows/main.cpp
@@ -0,0 +1,99 @@
+// Copyright 2019 Vlasov Andrey
+#include <gtest-mpi-listener.hpp>
+#include <gtest/gtest.h>
+#include "../../../modules/task_1/vlasov_a_min_value_rows/min_value_rows.h"
+
+TEST(min_value_rows_MPI, test1_negative_size_rows_on_matrix) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if (rank == 0) {
+	ASSERT_ANY_THROW(getRandomMatrix(-10, 10));
+  }
+}
+
+TEST(min_value_rows_MPI, test1_negative_size_columns_on_matrix) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if (rank == 0) {
+	ASSERT_ANY_THROW(getRandomMatrix(10, -10));
+  }
+}
+
+TEST(min_value_rows_MPI, test3_positive_values) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> M(25);
+  for (int i = 0; i < 25; i++)
+	M[i] = i;
+  std::vector<int> M_min = { 0, 5, 10, 15, 20 };
+  std::vector<int> rez = MinValuesRowsParallel(M, 5, 5);
+  if (rank == 0) {
+	EXPECT_EQ(rez, M_min);
+  }
+}
+
+TEST(min_value_rows_MPI, test4_mixed_values) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> M(9);
+  M[0] = -2;
+  M[1] = -1;
+  M[2] = 5;
+  M[3] = 7;
+  M[4] = 1;
+  M[5] = 10;
+  M[6] = -3;
+  M[7] = -1;
+  M[8] = -5;
+  std::vector<int> M_min = { -2, 1, -5 };
+  std::vector<int> rez = MinValuesRowsParallel(M, 3, 3);
+  if (rank == 0) {
+	EXPECT_EQ(rez, M_min);
+  }
+}
+
+TEST(min_value_rows_MPI, test5_negative_values) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> M(9);
+  M[0] = -5;
+  M[1] = -100;
+  M[2] = -30;
+  M[3] = -90;
+  M[4] = -10;
+  M[5] = -1;
+  M[6] = -70;
+  M[7] = -1;
+  M[8] = -500;
+  std::vector<int> M_min = { -100, -90, -500 };
+  std::vector<int> rez = MinValuesRowsParallel(M, 3, 3);
+  if (rank == 0) {
+	EXPECT_EQ(rez, M_min);
+  }
+}
+
+TEST(min_value_rows_MPI, test6_one_size) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> M = { 7 };
+  std::vector<int> M_min = { 7 };
+  std::vector<int> rez = MinValuesRowsParallel(M, 1, 1);
+  if (rank == 0) {
+	EXPECT_EQ(rez, M_min);
+  }
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  MPI_Init(&argc, &argv);
+
+  ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+  ::testing::TestEventListeners& listeners =
+	::testing::UnitTest::GetInstance()->listeners();
+
+  listeners.Release(listeners.default_result_printer());
+  listeners.Release(listeners.default_xml_generator());
+
+  listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+  return RUN_ALL_TESTS();
+} 
---
+++ modules/task_1/vlasov_a_min_value_rows/min_value_rows.h
@@ -0,0 +1,9 @@
+// Copyright 2019 Vlasov Andrey
+#ifndef MODULES_TASK_1_VLASOV_A_MIN_VALUE_ROWS_MIN_VALUE_ROWS_H
+#define MODULES_TASK_1_VLASOV_A_MIN_VALUE_ROWS_MIN_VALUE_ROWS_H
+#include <vector>
+
+std::vector<int> getRandomMatrix(int m, int n);
+std::vector<int> MinValuesRowsParallel(std::vector<int> &matrix, int m, int n);
+
+#endif  // MODULES_TASK_1_VLASOV_A_MIN_VALUE_ROWS_MIN_VALUE_ROWS_H
