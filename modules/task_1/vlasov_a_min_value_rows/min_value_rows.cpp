35d0964c5f244caea4c86b16dbae5b829390c364
381706-1VlasovAndrey <andrio.vlasov.2000@mail.ru>
2019-10-18 01:58:58 +0300

    return travis

--- modules/task_1/vlasov_a_min_value_rows/min_value_rows.cpp
+++ modules/task_1/vlasov_a_min_value_rows/min_value_rows.cpp
@@ -18,11 +18,19 @@ std::vector<int> getRandomMatrix(int m, int n) {
   }
 }
 
-std::vector<int> MinValuesRowsParallel(const std::vector<int> &matrix,
-  int m, int n) {
-  int size, rank, local_m, i, j;
-  i = 0;
-  j = 0;
+std::vector<int> MinValuesRowsSequential(const std::vector<int> &matrix, int m, int n) {
+  std::vector<int> rez(m);
+  for (int i = 0; i < m; i++) {
+    rez[i] = matrix[i*n];
+    for (int j = 1; j < n; j++)
+      if (matrix[i*n + j] < rez[i])
+        rez[i] = matrix[i*n + j];
+  }
+  return rez;
+}
+
+std::vector<int> MinValuesRowsParallel(const std::vector<int> &matrix, int m, int n) {
+  int size, rank, local_m;
   std::vector<int> rez(m);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -30,14 +38,22 @@ std::vector<int> MinValuesRowsParallel(const std::vector<int> &matrix,
   std::vector<int> local_rez(local_m);
   std::vector<int> local_matrix(local_m * n);
   MPI_Scatter(&matrix[0], local_m * n, MPI_INT, &local_matrix[0], local_m * n, MPI_INT, 0, MPI_COMM_WORLD);
-  for (i = 0; i < local_m; i++) {
+  for (int i = 0; i < local_m; i++) {
     local_rez[i] = local_matrix[i*n];
-    for (j = 1; j < n; j++) {
+    for (int j = 1; j < n; j++) {
       if (local_matrix[i*n + j] < local_rez[i]) {
         local_rez[i] = local_matrix[i*n + j];
       }
     }
   }
   MPI_Gather(&local_rez[0], local_m, MPI_INT, &rez[0], local_m, MPI_INT, 0, MPI_COMM_WORLD);
+  if ((rank == 0) & (m % size != 0)) {
+    for (int i = local_m * size; i < m; i++) {
+      rez[i] = matrix[i*n];
+      for (int j = 1; j < n; j++)
+        if (matrix[i*n + j] < rez[i])
+          rez[i] = matrix[i*n + j];
+    }
+  }
   return rez;
 }
--- modules/task_1/vlasov_a_min_value_rows/min_value_rows.h
+++ modules/task_1/vlasov_a_min_value_rows/min_value_rows.h
@@ -6,5 +6,6 @@
 
 std::vector<int> getRandomMatrix(int m, int n);
 std::vector<int> MinValuesRowsParallel(const std::vector<int> &matrix, int m, int n);
+std::vector<int> MinValuesRowsSequential(const std::vector<int> &matrix, int m, int n);
 
 #endif  // MODULES_TASK_1_VLASOV_A_MIN_VALUE_ROWS_MIN_VALUE_ROWS_H_
